5714b7116544b62acc5b9e14152c26c5a07cbe8a
Kiều <kieudpqn@yahoo.com>
2024-11-25 20:51:34 +0700

    completed counter

---
+++ counter/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function MyApp() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            
+
+        </div>
+    );
+}
